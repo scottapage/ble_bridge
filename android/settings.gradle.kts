a1e3fdb0076efc3c1fc603ae4ee2f94954da64a4
Scott Page <scottapage@gmail.com>
2025-08-11 15:06:59 +1200

    Fix settings.gradle.kts to load Flutter Gradle plugin

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,6 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 pluginManagement {
     repositories {
         google()
@@ -5,6 +8,22 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+val flutterSdkPath = System.getenv("FLUTTER_ROOT") ?: run {
+    val props = Properties()
+    val localProps = File(rootDir, "local.properties")
+    if (localProps.exists()) {
+        props.load(FileInputStream(localProps))
+        props.getProperty("flutter.sdk")
+    } else null
+} ?: throw GradleException("Flutter SDK not found. Set FLUTTER_ROOT or define flutter.sdk in android/local.properties")
+
+includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
+plugins {
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -12,5 +31,6 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
 rootProject.name = "ble_bridge"
 include(":app")
